dd725b3a4690a053af9b0df1ca9d7b789e070026
IDerr <ibrahim@derraz.fr>
2021-11-05 11:20:24 +0100

    ⬆ Adding up status to know if scrape goes well

--- internal/collector/eth_block_number.go
+++ internal/collector/eth_block_number.go
@@ -1,14 +1,17 @@
 package collector
 
 import (
+	"fmt"
+
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type EthBlockNumber struct {
-	rpc  *rpc.Client
-	desc *prometheus.Desc
+	rpc    *rpc.Client
+	desc   *prometheus.Desc
+	upDesc *prometheus.Desc
 }
 
 func NewEthBlockNumber(rpc *rpc.Client) *EthBlockNumber {
@@ -20,20 +23,29 @@ func NewEthBlockNumber(rpc *rpc.Client) *EthBlockNumber {
 			nil,
 			nil,
 		),
+		upDesc: prometheus.NewDesc(
+			"eth_block_up",
+			"has the rpc call succeded",
+			nil,
+			nil,
+		),
 	}
 }
 
 func (collector *EthBlockNumber) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.desc
+	ch <- collector.upDesc
 }
 
 func (collector *EthBlockNumber) Collect(ch chan<- prometheus.Metric) {
 	var result hexutil.Uint64
 	if err := collector.rpc.Call(&result, "eth_blockNumber"); err != nil {
-		ch <- prometheus.NewInvalidMetric(collector.desc, err)
+		ch <- prometheus.MustNewConstMetric(collector.upDesc, prometheus.GaugeValue, 0)
+		fmt.Println(err)
 		return
 	}
 
 	value := float64(result)
 	ch <- prometheus.MustNewConstMetric(collector.desc, prometheus.GaugeValue, value)
+	ch <- prometheus.MustNewConstMetric(collector.upDesc, prometheus.GaugeValue, 1)
 }
